ac4a4df9f5d1d066d95ab72b722342974121b643
Gowtham Gurrala <gurralagowtham@gmail.com>
2024-10-25 09:09:09 +0530

    Update HomeController.java
    add home  endpoint

--- src/main/java/com/Gtech/jenkins_Docker_app/controller/HomeController.java
+++ src/main/java/com/Gtech/jenkins_Docker_app/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("companyName", "Gtech Technologies");
         model.addAttribute("description", "Innovative solutions for your tech needs");
